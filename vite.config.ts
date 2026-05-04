d73c3a8695f3452b58a5681304e450d415dc387c
Andras Schmelczer <andras@schmelczer.dev>
2026-05-04 10:55:43 +0100

    Remove legacy CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 import { defineConfig } from 'vitest/config';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
+const cssTargets = browserslistToTargets(browserslist());
+
 export default defineConfig({
   plugins: [viteSingleFile()],
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: cssTargets,
+    },
+  },
   build: {
     target: 'es2022',
     cssCodeSplit: false,
+    cssMinify: 'lightningcss',
     assetsInlineLimit: Number.MAX_SAFE_INTEGER,
   },
   server: {
